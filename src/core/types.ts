fd2a70e41b3902a8cd44a85d1504c40069fe9949
yogesh201sits <yogeshjamdade201@gmail.com>
2026-07-08 17:37:21 +0530

    fix imporved implementation close to zustand

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,11 +1,20 @@
+export type SetState<T> = (
+  update: Partial<T> | ((state: T) => Partial<T>)
+) => void;
+
+export type GetState<T> = () => T;
+
 export type Listener<T> = (state: T) => void;
 
+export type Unsubscribe = () => void;
+
+export type StateCreator<T> = (
+  set: SetState<T>,
+  get: GetState<T>
+) => T;
+
 export interface StoreApi<T> {
-  getState(): T;
-
-  setState(
-    partial: Partial<T> | ((state: T) => Partial<T>)
-  ): void;
-
-  subscribe(listener: Listener<T>): () => void;
+  getState: GetState<T>;
+  setState: SetState<T>;
+  subscribe(listener: Listener<T>): Unsubscribe;
 }
