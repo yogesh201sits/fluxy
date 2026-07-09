c3a8e6ea33f47d08a9820d5a479745e292d10577
yogesh201sits <yogeshjamdade201@gmail.com>
2026-07-09 10:28:01 +0530

    feat(core): add StateCreator type for store initialization

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,12 +1,15 @@
 export type Listener<T> = (state: T) => void;
 
+export type StateCreator<T> = (
+  set: StoreApi<T>["setState"],
+  get: StoreApi<T>["getState"]
+) => T;
 
 export interface StoreApi<T> {
   getState(): T;
 
-  setState(
-    partial: Partial<T> | ((state: T) => Partial<T>)
-  ): void;
+  setState(partial: Partial<T> | ((state: T) => Partial<T>)): void;
 
   subscribe(listener: Listener<T>): () => void;
+  
 }
